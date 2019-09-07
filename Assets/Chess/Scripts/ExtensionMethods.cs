c115bbf7b6ca93133ab359eec6cad14c76f243d8
Stuart Buchbinder <stubuchbinder79@gmail.com>
2019-09-07 09:36:19 -0500

    extensions to convert from Vector2Int to Vector3 and vice versa

---
+++ Assets/Chess/Scripts/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class ExtensionMethods 
+{
+    public static Vector2Int Vector2IntFromVector3(this Vector3 point)
+    {
+        int col = Mathf.FloorToInt(4f + point.x);
+        int row = Mathf.FloorToInt(4f + point.z);
+        return new Vector2Int(col, row);
+    }
+
+    public static Vector3 Vector3FromVector2Int(this Vector2Int gridPoint)
+    {
+        float x = -3.5f + 1.0f * gridPoint.x;
+        float z = -3.5f + 1.0f * gridPoint.y;
+        return new Vector3(x, 0, z);
+    }
+}
